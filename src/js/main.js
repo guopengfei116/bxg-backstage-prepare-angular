442b1bcb620b466962e765f5f2819f121bef1d2f
PengfeiGuo <626138959@qq.com>
2017-08-29 17:50:23 +0800

    添加路由配置，实现首页与登陆页的切换

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,22 @@
-angular.module('app', []);
+angular.module('app', [
+  'ngRoute', 
+  'aside', 
+  'header', 
+  'index', 
+  'login'
+])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+          template: '<ngl-aside></ngl-aside><ngl-index></ngl-index>'
+      })
+      .when('/index', {
+          redirectTo: '/'
+      })
+      .when('/login', {
+          template: '<ngl-login></ngl-login>'
+      })
+      .otherwise({
+        templateUrl: '/src/html/404.html'
+      })
+  }])
